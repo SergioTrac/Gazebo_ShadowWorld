bcf43a788aab4e608e9dc229d424a5226f62a242
SergioTrac <sergiotraceslava@gmail.com>
2023-01-23 11:59:38 +0100

    UPDATE - Modificados algunos valores de tiempo en el .sh del Yakuake.

--- complete_edge.sh
+++ complete_edge.sh
@@ -26,35 +26,35 @@ qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 0 "rcnode&"
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 0 "cd ~/Repositories/Gazebo_ShadowWorld"
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 0 "gz sim shadow.sdf"
 
-sleep 4
+sleep 6
 
 #---------------------Gazebo2Robocomp------------------------
 
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 1 "cd ~/Repositories/gazebo-bridge/"
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 1 "bin/Gazebo2Robocomp etc/config"
 
-sleep 0.5
+sleep 1
 
 #---------------------LASER VIEWER------------------------
 
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 2 "cd ~/robocomp/components/robocomp-robolab/components/viewers/laser_viewer"
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 2 "bin/laser_viewer etc/gazebo_config"
 
-sleep 0.5
+sleep 1
 
 #---------------------JOYSTICK PUBLISHER------------------------
 
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 3 "cd ~/robocomp/components/robocomp-robolab/components/hardware/external_control/joystickpublish"
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 3 "bin/JoystickPublish etc/config_gazebo"
 
-sleep 0.5
+sleep 1
 
 #---------------------CAMERA RGBD SIMPLE------------------------
 
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 4 "cd ~/robocomp/components/robocomp-robolab/components/viewers/camera_rgbdsimple_viewer"
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 4 "bin/camera_rgbdsimple_viewer etc/gazebo_config"
 
-sleep 0.5
+sleep 1
 
 #----------------------------------------------------------------------------------------------------#
 
